47434d2c21c833d710957d5f52ade948600f433d
jhupo <hi.jhupo@gmail.com>
2026-08-26 20:24:39 +0800

    chore(release): bump version to 0.4.102

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -20666,7 +20666,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.4.101",
+	Version:          "0.4.102",
 	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
